3ed964b9f3f3dd45c6a08be424ee24538068732c
mojtaba <mojtaba@gmail.com>
2023-02-09 16:21:16 +0330

    add action for active command

--- pages/admin.py
+++ pages/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Command
 
-admin.site.register(Command)
+
+@admin.action(description='فعال کردن نظر')
+def activer(modeladmin, request, queryset):
+    queryset.update(is_active=True)
+
+
+class CommendActive(admin.ModelAdmin):
+    actions = [activer]
+
+
+admin.site.register(Command, CommendActive)
