f9cbc38cb03a7c8806dbb3b0e4244fcea68121de
mojtaba <mojtaba@gmail.com>
2023-02-12 12:43:07 +0330

    add name for url in urls.py accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,20 +2,15 @@ from django.urls import path
 from . import views
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 app_name = 'accounts'
 
 urlpatterns = [
-    path('register/', views.RegisterAPIView.as_view()),
-    path('login/', views.LoginAPIView.as_view()),
-    path('logout/', views.LogoutAPIView.as_view()),
-    path('profile/', views.ProfileShowAPIView.as_view()),
-    path('update/profile/<int:pk>/', views.ProfileUpdateAPIView.as_view()),
+    path('register/', views.RegisterAPIView.as_view(), name='register'),
+    path('login/', views.LoginAPIView.as_view(), name='login'),
+    path('logout/', views.LogoutAPIView.as_view(), name='logout'),
+    path('profile/', views.ProfileShowAPIView.as_view(), name='profile'),
+    path('update/profile/<int:pk>/', views.ProfileUpdateAPIView.as_view(), name='update_profile'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
-
-
-
-
-
-
