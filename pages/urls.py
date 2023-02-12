147b27797c0c3f7ee2148958f72c5d2201d4276b
mojtaba <mojtaba@gmail.com>
2023-02-12 11:29:01 +0330

    add name url

--- pages/urls.py
+++ pages/urls.py
@@ -4,11 +4,12 @@ from . import views
 app_name = 'pages'
 
 urlpatterns = [
-    path('', views.FilmAPIView.as_view()),
-    path('detail/<str:slug_film>/', views.FilmDetailAPIView.as_view()),
-    path('search/', views.FilmSearchAPIView.as_view()),
-    path('filter/', views.FilterTypeAPIView.as_view()),
-    path('genre/<str:slug_genre>/', views.FilterGenreAPIView.as_view()),
-    path('add/command/<str:slug_film>/', views.CreateCommandAPIView.as_view()),
-    path('add/command/<str:slug_film>/<int:pk_command>/', views.CreateAnswerCommandAPIView.as_view()),
+    path('', views.FilmAPIView.as_view(), name='home'),
+    path('detail/<str:slug_film>/', views.FilmDetailAPIView.as_view(), name='detail'),
+    path('search/', views.FilmSearchAPIView.as_view(), name='search'),
+    path('filter/', views.FilterTypeAPIView.as_view(), name='filter'),
+    path('genre/<str:slug_genre>/', views.FilterGenreAPIView.as_view(), name='genre'),
+    path('add/command/<str:slug_film>/', views.CreateCommandAPIView.as_view(), name='add-commend'),
+    path('add/command/<str:slug_film>/<int:pk_command>/', views.CreateAnswerCommandAPIView.as_view(),
+         name='answer-commend'),
 ]
